a7a8013defc269cfed3f84ea8ffe2e4b8ee6ac37
fkunstner <frederik.kunstner@gmail.com>
2021-09-05 09:42:42 -0700

    Renaming mixup

--- dsdl/__init__.py
+++ dsdl/__init__.py
@@ -1,10 +1,10 @@
 from .dataset import load, is_downloaded
-from . import available_datasets
+from .available_datasets import DSETS
 from .config import Config
 
 
 def available_datasets():
-    return list(available_datasets.DSETS.keys())
+    return list(DSETS.keys())
 
 
 def loader(dsname):
